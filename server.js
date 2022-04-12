cf6d1bcd0b7ce017ef3e475d85131a4e72afc082
kundan kumar <kundan35708@gmail.com>
2022-04-12 18:57:53 +0530

    modified : server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const serverConfig = require("./configs/server.config");
-
+const mongoose =require("mongoose");
+const dbConfig = require("./configs/db.config");
 const app = express();
+
+/**
+ * setup the mongodb connection and create admin user
+ */
+mongoose.connect(dbConfig.DB_URL, ()=>{
+    console.log("MongoDB connected");
+});
 
 
 /* start the express server*/
